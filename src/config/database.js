f0cce1576ff7392acdefe0515f8f1d6d9f84649f
vivek-026 <vkbhamare26@gmail.com>
2025-07-12 16:02:38 +0530

    added mongo db atlas

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-const connect = async()=>{
-
-    await mongoose.connect('mongodb://localhost:27017/arena');
-    console.log("Mongo db connect")
-
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://arena_admin:arena%4026@cluster0.1vsgvte.mongodb.net/arena?retryWrites=true&w=majority&appName=Cluster0'
+    );
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connect;
